99a198c1913ad43810ba4ace45bdee9b671ed773
Arun Singh <aruns.ug21.cs@nitp.ac.in>
2023-04-16 07:34:07 +0530

    by-arun

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
+import LogInS from "./components/seller/login";
+import SignUpS from "./components/seller/signup";
 
+import LogInB from "./components/buyer/login";
+import SignUpB from "./components/buyer/signup";
+
+import Product from "./components/productdetails/product";
+import Equipment from "./components/service/farmequipment";
+
+import Expert from "./expert/expert";
 function App() {
   return (
     <>
-      <div>AgriCult</div>
+      {/* <SignUp /> */}
+
+      {/* <LogIn /> */}
+
+      {/* <LogInB /> */}
+
+      {/* <SignUpB /> */}
+
+      {/* <Product /> */}
+
+      {/* <Equipment /> */}
+
+      <Expert />
     </>
   );
 }
